08a410a109cd40cc9447407f4ee792f6b9065cba
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-07-01 14:44:12 +0000

    Post and Get from mirth

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ISISconnect
 ===========
+
+CURL
+-------
+To download fil with CURL
+curl -O -J <host>
+
+To Post cr formated text with curl
+curl -F filedata=@<filename> http://146.148.3.248:80/crtxtd/
--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-if (process.env.C9_PROJECT) {
-    require('./private.js');
-}
 var express = require("express"),
-    app = express()
+    app = express(),
+    mongoose = require('mongoose'),
+    db = mongoose.connection;
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
- 
+//add support for text/plain
+app.use(function(req, res, next) {
+
+    if (req.is('text/*')) {
+        req.text = '';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk) {
+            req.text += chunk;
+        });
+        req.on('end', next);
+    }
+    else {
+        next();
+    }
 });
+process.env.MONGOCON = "mongodb://cloud9:ZSTqSa04E7Lp3ao@kahana.mongohq.com:10005/app26722556";
 app.listen(process.env.PORT, process.env.IP);
-//
-app.get('/', function(req, res) {
-
+var testdata;
+//Start Mongoose
+mongoose.connect(process.env.MONGOCON);
+//DB open function
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+db.once('open', function callback() {
+    app.post('/', function(req, res) {
+        console.log(req.text);
+        testdata=req.text;
+        res.send(200);
+    });
+    app.get('/hello', function(req, res) {
+        //set the download name
+        res.setHeader('Content-disposition', 'attachment; filename=dramaticpenguin.txt');
+        //Send the datas ad a file
+        res.send(testdata);
+    });
 });
