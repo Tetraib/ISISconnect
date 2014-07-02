f6c46205ef3cecdb81e317c81db15214ab2073de
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2014-07-02 12:56:21 +0000

    Forward to mirth

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ To download fil with CURL
 curl -O -J <host>
 
 To Post cr formated text with curl
-curl -F filedata=@<filename> http://146.148.3.248:80/crtxtd/
+curl -H "exam_infos":"\"20140219131841\",\"18774\",\"TEST\",\"Osiris\",\"19320416\",\"ECHOGRAPHIE ABDOMINALE\",\"20140219131841_000005955.pdf\",\"000000000\",\"20/02/2014\"" -F filedata=@<filename> <host>/postcr
+uses \" to escape : "
--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require("express"),
     app = express(),
     mongoose = require('mongoose'),
-    db = mongoose.connection;
-app.use(express.urlencoded());
-app.use(express.json());
+    db = mongoose.connection,
+    request = require("request"),
+    multer = require('multer'),
+    bodyParser = require('body-parser');
 
+//Use for upload file
+app.use(multer({
+    dest: './uploads/'
+}));
+//use for parsing body data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 //add support for text/plain
 app.use(function(req, res, next) {
-
     if (req.is('text/*')) {
         req.text = '';
         req.setEncoding('utf8');
@@ -22,21 +30,40 @@ app.use(function(req, res, next) {
 });
 process.env.MONGOCON = "mongodb://cloud9:ZSTqSa04E7Lp3ao@kahana.mongohq.com:10005/app26722556";
 app.listen(process.env.PORT, process.env.IP);
+
+//just test for now
 var testdata;
+
+
 //Start Mongoose
 mongoose.connect(process.env.MONGOCON);
 //DB open function
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 db.once('open', function callback() {
-    app.post('/', function(req, res) {
+    //use to get the hl7 from mirth
+    app.post('/posthl7', function(req, res) {
         console.log(req.text);
-        testdata=req.text;
+        testdata = req.text;
+        res.send(200);
+    });
+    //use to post the file in body and the patient info in the header and forward it to mirth
+    app.post('/postcr', function(req, res) {
+        // console.log(req.files);
+        // Forward to mirth
+        request.post({
+            url: 'http://146.148.3.248:80/crtxt/',
+            body: req.headers.exam_infos,
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        });
         res.send(200);
     });
     app.get('/hello', function(req, res) {
         //set the download name
         res.setHeader('Content-disposition', 'attachment; filename=dramaticpenguin.txt');
-        //Send the datas ad a file
+        // Send the datas ad a file
+        console.log(testdata);
         res.send(testdata);
     });
 });
